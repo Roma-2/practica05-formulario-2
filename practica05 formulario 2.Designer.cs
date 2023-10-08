30f8e90e005bb4b1972913245e06123335f845f7
Roma-2 <daniel.romano43@unach.mx>
2023-10-08 17:31:49 -0600

    Agregar archivos de proyecto.

---
+++ practica05 formulario 2.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtEstatura = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.rbHombre = new System.Windows.Forms.RadioButton();
+            this.rbMujer = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(27, 47);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(100, 20);
+            this.txtNombres.TabIndex = 0;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(158, 47);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidos.TabIndex = 1;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(287, 47);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // txtEstatura
+            // 
+            this.txtEstatura.Location = new System.Drawing.Point(420, 46);
+            this.txtEstatura.Name = "txtEstatura";
+            this.txtEstatura.Size = new System.Drawing.Size(100, 20);
+            this.txtEstatura.TabIndex = 3;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(556, 46);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 4;
+            // 
+            // rbHombre
+            // 
+            this.rbHombre.AutoSize = true;
+            this.rbHombre.Location = new System.Drawing.Point(73, 115);
+            this.rbHombre.Name = "rbHombre";
+            this.rbHombre.Size = new System.Drawing.Size(62, 17);
+            this.rbHombre.TabIndex = 5;
+            this.rbHombre.Text = "Hombre";
+            this.rbHombre.UseVisualStyleBackColor = true;
+            // 
+            // rbMujer
+            // 
+            this.rbMujer.AutoSize = true;
+            this.rbMujer.Checked = true;
+            this.rbMujer.Location = new System.Drawing.Point(214, 115);
+            this.rbMujer.Name = "rbMujer";
+            this.rbMujer.Size = new System.Drawing.Size(51, 17);
+            this.rbMujer.TabIndex = 6;
+            this.rbMujer.TabStop = true;
+            this.rbMujer.Text = "Mujer";
+            this.rbMujer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(867, 314);
+            this.Controls.Add(this.rbMujer);
+            this.Controls.Add(this.rbHombre);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtEstatura);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtEstatura;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.RadioButton rbHombre;
+        private System.Windows.Forms.RadioButton rbMujer;
+    }
+}
+
---
+++ practica05 formulario 2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombres = txtNombres.Text;
+            string apellidos = txtApellidos.Text;
+            string edad = txtEdad.Text;
+            string estatura = txtEstatura.Text;
+            string telefono = txtTelefono.Text;
+
+            string genero = "";
+
+            if (rbHombre.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (rbMujer.Checked)
+            {
+                genero = "Mujer";
+            }
+
+            if (EsEnteroValido(edad) && EsDecimalValido(estatura) && EsEnteroValidoDe10Digitos(telefono) &&
+                EsTextoValido(nombres) && EsTextoValido(apellidos))
+            {
+                string datos = $"Nombres: {nombres}\nApellidos: {apellidos}\nEdad: {edad} años\nEstatura: {estatura} cm\nTelefono: {telefono}\nGenero: {genero}";
+
+                using (StreamWriter archivo = File.AppendText("datos99.txt"))
+                {
+                    archivo.WriteLine(datos + "\n\n");
+                }
+
+                MessageBox.Show("Datos Guardados con éxito: \n\n" + datos, "Información");
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos válidos en los campos.", "Error");
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombres.Clear();
+            txtApellidos.Clear();
+            txtEdad.Clear();
+            txtEstatura.Clear();
+            txtTelefono.Clear();
+            rbHombre.Checked = false;
+            rbMujer.Checked = false;
+        }
+
+        private bool EsEnteroValido(string valor)
+        {
+            return int.TryParse(valor, out _);
+        }
+
+        private bool EsDecimalValido(string valor)
+        {
+            return double.TryParse(valor, out _);
+        }
+
+        private bool EsEnteroValidoDe10Digitos(string valor)
+        {
+            return valor.Length == 10 && long.TryParse(valor, out _);
+        }
+
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, "^[a-zA-Z\\s]+$");
+        }
+    }
+}
